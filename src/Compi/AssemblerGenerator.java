3c0b823f72a28eb4b2e4f4ab70905904321cdc39
camicalca <camicalcagno12@gmail.com>
2016-10-19 10:43:37 -0300

    asembler

--- src/Compi/AssemblerGenerator.java
+++ src/Compi/AssemblerGenerator.java
@@ -1,12 +1,51 @@
 public class AssemblerGenerator{
-	/*LinkedList<String> textSegment = new LinkedList<String>();
+	LinkedList<String> textSegment = new LinkedList<String>();
 	LinkedList<String> dataSegment = new LinkedList<String>();
 
 	public String readList(LinkedList<IntermediateCode> l){
 
 		for(IntermediateCode i : l){
 			switch(i.getOperator()){
-				case "SUM": 
+				case SUM: 
+					sum(i);
+				case RES:
+					res(i);
+				case MUL:
+					mul(i);
+				case DIV:
+					div(i);
+				case MAY:
+					may(i);
+				case MEN:
+					men(i);
+				case MAYI:
+					mayi(i);
+				case MENI:
+					meni(i);
+				case IGUAL:
+					igual(i);
+				case AND:
+					and(i);
+				case OR:
+					or(i);
+				case RESERVE:
+					reserve(i);
+				case CALL:
+					call(i);
+				case ASIGN:
+					asign(i);
+				case ASIGNMAS:
+					asignmas(i);
+				case ASIGNMENOS:
+					asignmenos(i);
+				case JMPBREAK:
+					jmpbreak(i);
+				case JMPCONTINUE:
+					jmpcontinue(i);
+				
+
+
+				
 
 
 
@@ -29,6 +68,6 @@ public class AssemblerGenerator{
 
 
 
-	}*/
+	}
 
 }
