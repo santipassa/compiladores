83a07d1c4e3239c4a0d1ac0e8142d5bc4c0994d5
camicalca <camicalcagno12@gmail.com>
2016-11-16 11:02:49 -0300

    Entrega e Informe

--- testPrueba.sh
+++ testPrueba.sh
@@ -1,21 +1,5 @@
 #!/bin/bash
 echo "=====================================\n"
-echo "========COMPILANDO EL JFLEX==========\n"
-echo "=====================================\n"
-java -jar lib/JFlex.jar -d src/Compi jflex/Scanner.jflex
-echo "=====================================\n"
-echo "========COMPILANDO EL CUP============\n"
-echo "=====================================\n"
-java -jar tools/java-cup-11b.jar -interface -locations -parser Parser -destdir src/Compi cup/Parser.cup
-#export CLASSPATH=""
-#for file in 'lib/'; do export CLASSPATH=$CLASSPATH:lib/$file; done
-#echo $CLASSPATH 
-echo "=====================================\n"
-echo "========COMPILANDO LOS .JAVA=========\n"
-echo "=====================================\n"
-javac -cp .:lib/java-cup-11b-runtime.jar src/Compi/*.java -d bin/
-
-echo "=====================================\n"
 echo "============EJECUTANDO===============\n"
 echo "=====================================\n"
 
